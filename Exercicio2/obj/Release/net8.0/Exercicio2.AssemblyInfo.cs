e64b5a2353ebe823964cca5af5ed277ab49bb149
Rollins15 <rollinschanesa@gmail.com>
2025-08-11 09:37:59 +0200

    nova versao 09h21, 11/08/2025

--- Exercicio2/obj/Release/net8.0/Exercicio2.AssemblyInfo.cs
+++ Exercicio2/obj/Release/net8.0/Exercicio2.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fd204225c88e5c6eb4d076fcd03710ed54fd5e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio2")]
